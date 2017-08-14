23ce28664ab60c8550ccd98f0eac55e217406df8
Stephen Taylor <me@meandmycode.com>
2017-08-14 17:13:31 +0100

    project fundamentals

---
+++ scripts/build.js
@@ -0,0 +1,51 @@
+/* global process */
+import { writeFileSync } from 'fs';
+import minimist from 'minimist';
+import webpack from 'webpack';
+
+import configure from '../webpack.config';
+
+const args = minimist(process.argv.slice(2));
+
+const outputPath = args['out-dir'];
+
+const appConfig = {
+};
+
+const config = configure({ production: true, outputPath, appConfig });
+
+const statsOptions = {
+    colors: true,
+};
+
+webpack(config, (err, stats) => {
+
+    const hasErrors = err != null || (stats && stats.hasErrors());
+
+    if (err) {
+
+        console.error(err.stack || err);
+
+        if (err.details) console.error(err.details);
+
+    }
+
+    if (stats) process.stdout.write(stats.toString(statsOptions));
+
+    if (hasErrors) {
+
+        console.log('\n');
+        console.log('✖️  Web application build failed!');
+        console.log('\n');
+
+        return process.exit(1);
+
+    }
+
+    console.log('\n');
+    console.log('🌟  Web application build successful!');
+    console.log('\n');
+
+    writeFileSync('stats.json', JSON.stringify(stats.toJson()), null, 2);
+
+});
