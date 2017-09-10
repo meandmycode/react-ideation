515055fe1b981c05d74e442e5a7f488e74a76e06
Unknown <me@meandmycode.com>
2017-09-10 16:59:36 +0100

    refactor ideas actions to use dependency middleware instead of thunk, we also switch from an upsert function to distinct create/update functions and require a trackingKey (previously ephemeral) to be passed to us (this partially fixes an issue with our reducer not being pure due to generating random keys itself)

--- src/actions/ideas.js
+++ src/actions/ideas.js
@@ -1,60 +1,44 @@
 import { LIST_IDEAS, UPSERT_IDEA, REMOVE_IDEA } from '../constants/action-types';
 
-export const list = () => (dispatch, getState, { ideasService }) => {
+export const list = () => ({ ideasService }) => ({
+    type: LIST_IDEAS,
+    payload: ideasService.list(),
+});
 
-    dispatch({
-        type: LIST_IDEAS,
-        payload: ideasService.list(),
-    });
+export const create = (item, trackingKey, flush) => ({ ideasService }) => {
+
+    const payload = flush && ideasService.create(item);
+
+    return {
+        type: UPSERT_IDEA,
+        item,
+        trackingKey,
+        payload,
+    };
 
 };
 
-export const upsert = (item, entity, flush) => (dispatch, getState, { ideasService }) => {
+export const update = (item, entity, flush) => ({ ideasService }) => {
 
-    const type = UPSERT_IDEA;
+    const payload = flush && ideasService.update(entity.key, item);
 
-    if (flush) {
-
-        const payload = entity && entity.key
-            ? ideasService.update(entity.key, item)
-            : ideasService.create(item);
-
-        return dispatch({
-            type,
-            item,
-            entity,
-            payload,
-        });
-
-    }
-
-    dispatch({
-        type,
+    return {
+        type: UPSERT_IDEA,
         item,
         entity,
-    });
+        payload,
+    };
 
 };
 
-export const remove = (entity, flush) => (dispatch, getState, { ideasService }) => {
+export const remove = (entity, flush) => ({ ideasService }) => {
 
-    const type = REMOVE_IDEA;
+    const payload = flush && ideasService.remove(entity.key);
 
-    if (flush) {
-
-        const payload = ideasService.remove(entity.key);
-
-        return dispatch({
-            type,
-            entity,
-            payload,
-        });
-
-    }
-
-    dispatch({
-        type,
+    return {
+        type: REMOVE_IDEA,
         entity,
-    });
+        payload,
+    };
 
 };
